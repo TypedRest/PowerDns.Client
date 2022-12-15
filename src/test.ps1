1ef0576f6cf1d959c18db869fee45d0fb35e6a0f
Bastian Eicher <bastian@eicher.net>
2022-12-15 18:00:22 +0100

    Replaced deprecated GitHub Actions

--- src/test.ps1
+++ src/test.ps1
@@ -2,10 +2,10 @@
 pushd $PSScriptRoot
 
 function Run-DotNet {
-    ..\0install.ps1 run --batch --version 6.0.. https://apps.0install.net/dotnet/sdk.xml @args
+    ..\0install.ps1 run --batch --version 6.0..!6.1 https://apps.0install.net/dotnet/sdk.xml @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
-Run-DotNet test --no-build --logger junit --configuration Release UnitTests\UnitTests.csproj
+Run-DotNet test --no-build --logger trx --configuration Release UnitTests\UnitTests.csproj
 
 popd
